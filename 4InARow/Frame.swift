611c21c01e086ea36ccd939e10c1b3dce666736c
Ari Jain <thearijain@gmail.com>
2020-02-08 20:15:58 -0500

    Initial

---
+++ 4InARow/Frame.swift
@@ -0,0 +1,209 @@
+//
+//  Frame.swift
+//  4InARow
+//
+//  Created by Ari Jain on 2/7/20.
+//  Copyright © 2020 Arihant Jain. All rights reserved.
+//
+
+import UIKit
+
+enum Chip: Int {
+    case null = 0
+    case pink
+    case green
+}
+
+class Frame: NSObject {
+    
+    let width = 7
+    let height = 6
+    var currentChip: Chip
+    var currentChipName: String!
+    var playerChip: Chip = Chip.pink
+    var computerChip: Chip = Chip.green
+    
+
+    //2D Backing Array
+    var spaces = [[Chip]]()
+    
+    override init() {
+        spaces = Array(repeating: Array(repeating: Chip.null, count: height), count: width)
+        
+        //randomizes who gets the first move
+        //MIGHT HAVE TO REMOVE THIS LATER
+//        let initialPlayer = Int.random(in: 0..<2)
+//        if initialPlayer == 1 {
+//            currentChip = computerChip
+//            currentChipName = "greenChip"
+//        } else {
+//            currentChip = playerChip
+//            currentChipName = "pinkChip"
+//        }
+        currentChip = Chip.pink
+        currentChipName = "pinkChip"
+        
+        super.init()
+    }
+    
+    //Checks if the frame is full
+    func isFrameFull() -> Bool {
+        for i in 0..<width {
+            if spaces[i][5] == Chip.null {
+                return false
+            }
+        }
+        return true;
+    }
+    
+    //Iterates through passed in column, checks if any row is available
+    func isRowAvailable(column: Int) -> Int? {
+        for row in (0..<height).reversed() {
+            if spaces[column][row] == Chip.null {
+                return row
+            }
+        }
+        return nil
+    }
+    
+    //Checks if the passed in column is full
+    func isColumnFull(column: Int) -> Bool {
+        return isRowAvailable(column: column) != nil
+    }
+    
+    //Sets the 2D array at a certain point to the currentChip
+    func set(column: Int) {
+        if let row = isRowAvailable(column: column) {
+            spaces[column][row] = currentChip
+            print(isWin(Row: row, Column: column, ChipColor: currentChip))
+        }
+        
+        //PRINTOUT ARR
+        for i in 0..<height {
+            for j in 0..<width {
+                print(spaces[j][i], "   ", terminator : "")
+            }
+            print(" ")
+        }
+        
+    }
+    
+    //Switches the chip colors
+    func switchColor() {
+        if (currentChip == playerChip) {
+            currentChip = computerChip
+            currentChipName = "greenChip"
+        } else {
+            currentChip = playerChip
+            currentChipName = "pinkChip"
+        }
+    }
+    
+    
+    func isWin(Row row: Int, Column column: Int, ChipColor: Chip) -> Bool {
+        print(column, ", ", row)
+        
+        //Checks if atleast 7 chips have been played
+        if (ViewController.chipsPlayed < 6) { return false }
+        
+        //Checks right horizontally
+        var count = 1
+        for i in 1..<4 {
+            if column + 3 > width - 1 {
+                break
+            }
+            if spaces[column + i][row] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        //Checks left horizontally
+        count = 1
+        for i in 1..<4 {
+           if column - 3 < 0 {
+               break
+           }
+           if spaces[column - i][row] == ChipColor {
+               count += 1
+           }
+        }
+        if count == 4 { return true }
+        
+        //Checks upward vertically
+        count = 1
+        for i in 1..<4 {
+          if row + 3 > height - 1 {
+              break
+          }
+          if spaces[column][row + i] == ChipColor {
+              count += 1
+          }
+       }
+       if count == 4 { return true }
+        
+        
+        //Checks downward vertically
+        count = 1
+        for i in 1..<4 {
+            if row - 3 < 0 {
+                break
+            }
+            if spaces[column][row - i] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        //Checks upright diagonol
+        count = 1
+        for i in 1..<4 {
+            if ((column + 3 > width - 1) || (row + 3 > height - 1)) {
+                break
+            }
+            if spaces[column + i][row + i] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        //Checks downright diagonal
+        count = 1
+        for i in 1..<4 {
+            if ((column + 3 > width - 1) || (row - 3 < 0)) {
+                break
+            }
+            if spaces[column + i][row - i] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        //Checks upleft diagonal
+        count = 1
+        for i in 1..<4 {
+            if ((column - 3 < 0) || (row + 3 > height - 1)) {
+                break
+            }
+            if spaces[column - i][row + i] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        //Checks downleft diagonal
+        count = 1
+        for i in 1..<4 {
+            if ((column - 3 < 0) || (row - 3 < 0)) {
+                break
+            }
+            if spaces[column - i][row - i] == ChipColor {
+                count += 1
+            }
+        }
+        if count == 4 { return true }
+        
+        return false
+    }
+
+}
--- 4InARow/ViewController.swift
+++ 4InARow/ViewController.swift
@@ -10,11 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    //Array of column buttons
+    @IBOutlet var colButtons: [UIButton]!
+    
+    static var chipsPlayed = 0
+    var frame: Frame!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
+        frame = Frame()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func play(_ sender: UIButton) {
+        let column  = sender.tag
+        
+        if (frame.isColumnFull(column: column)) {
+            dropChip(chip: frame.currentChip, Column: column)
+            frame.set(column: column)
+            frame.switchColor()
+            
+            if (frame.currentChip == Chip.green) {
+                //Allows code to run after 1 second
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    self.opponentTurn()
+                }
+            }
+            
+        }
+    }
+        
+    func opponentTurn() {
+        let column = Int.random(in: 0..<7)
+        
+        if (frame.isColumnFull(column: column)) {
+            dropChip(chip: frame.currentChip, Column: column)
+            frame.set(column: column)
+            frame.switchColor()
+        }
     }
 
-
+    
+    func dropChip(chip: Chip, Column column: Int) {
+        let row = frame.isRowAvailable(column: column)!
+        let (xcoor, ycoor) = chipPosition(Column: column, Row: row)
+        let imageViewObj = UIImageView(frame: CGRect(x: xcoor, y: -chipSize() * 3, width: chipSize(), height: chipSize()))
+        imageViewObj.image = UIImage(named:frame.currentChipName!)
+        stackView.addSubview(imageViewObj)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseIn, animations:{
+            imageViewObj.frame.origin.y = ycoor
+        }, completion: nil)
+    }
+    
+    func chipPosition(Column col: Int, Row row: Int) -> (x: CGFloat, y: CGFloat) {
+        let button = colButtons[col]
+        let x = button.frame.midX - (chipSize() / 2)
+        let y = (button.frame.maxY - chipSize() / 2) - (chipSize() * CGFloat(frame.height - row))
+        return (CGFloat(x), CGFloat(y))
+    }
+    
+    
+    func chipSize() -> CGFloat {
+        return min(stackView.frame.height / 6 - 16, stackView.frame.width / 7 - 16)
+    }
 }
+
 
