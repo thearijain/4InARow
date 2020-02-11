b026e0882f015069cb292c494499d350217066ee
Ari Jain <thearijain@gmail.com>
2020-02-11 16:20:17 -0500

    fsrfd

--- 4InARowTests/_InARowTests.swift
+++ 4InARowTests/_InARowTests.swift
@@ -10,25 +10,46 @@ import XCTest
 @testable import _InARow
 
 class _InARowTests: XCTestCase {
+    var spaces = [[Chip]]()
+    let width = 7
+    let height = 6
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    
+    func testCheckBoardInitialized() {
+        spaces = Array(repeating: Array(repeating: Chip.null, count: height), count: width)
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        for i in 0..<height {
+            for j in 0..<width {
+               print(spaces[j][i], " ", terminator: "")
+            }
+           print("")
         }
     }
+    
+    func testSet() {
+        spaces = Array(repeating: Array(repeating: Chip.null, count: height), count: width)
+        let column = 3
+        let row = 4
+        spaces[column][row] = .pink
+        
+        for i in 0..<height {
+             for j in 0..<width {
+                print(spaces[j][i], " ", terminator: "")
+             }
+            print("")
+         }
+    }
+    
+    func testIsFrameFull() {
+        
+        for i in 0..<width {
+            if spaces[i][0] == Chip.null {
+                print (false)
+            }
+        }
+        print (true)
+    }
+    
+//I would have written tests for my diagonal method but running the project and creating the diagonals was more fun!
 
 }
